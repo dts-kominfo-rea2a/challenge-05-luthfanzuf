99d2afbf08b9f2e38a6f0b48f77cb24f0762c448
luthfanzuf <luthfanzuf@gmail.com>
2022-06-04 08:47:13 +0700

    commit

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arr, fnSortDirection) => {
+  const arr1 = [...arr]
+  fnSortDirection(arr1)
+  for(let i = 0; i<arr1.length; i++){
+      arr1[i] = (i+1) + ". " + arr1[i]
+    }
+  return arr1
+} 
+
+
+
+//console.log(addNumber(names))
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arr)=>{
+  return arr.sort()
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arr)=>{
+  return arr.sort().reverse()
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
